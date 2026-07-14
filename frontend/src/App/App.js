b274d57563c37537736b3cdb36b015a0ddec25d2
Mark Vital <mark@vital.com>
2026-07-14 18:29:53 -0500

    Restore original Readarr frontend, remove custom manga UI files

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,43 +1,31 @@
+import { ConnectedRouter } from 'connected-react-router';
+import PropTypes from 'prop-types';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MangaSeriesList from './MangaSeries/MangaSeriesList';
-import MangaSearch from './MangaSearch/MangaSearch';
-import VolumeList from './VolumeList/VolumeList';
-import MangaSettings from './MangaSettings/MangaSettings';
+import DocumentTitle from 'react-document-title';
+import { Provider } from 'react-redux';
+import PageConnector from 'Components/Page/PageConnector';
+import ApplyTheme from './ApplyTheme';
+import AppRoutes from './AppRoutes';
 
-const App = () => {
+function App({ store, history }) {
   return (
-    <Router>
-      <div className="app">
-        {/* Navigation */}
-        <nav className="navbar">
-          <div className="navbar-brand">
-            <Link to="/">MangaArr</Link>
-          </div>
-          <div className="navbar-menu">
-            <Link to="/" className="navbar-item">Library</Link>
-            <Link to="/add" className="navbar-item">Add Manga</Link>
-            <Link to="/settings" className="navbar-item">Settings</Link>
-          </div>
-        </nav>
-
-        {/* Main Content */}
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<MangaSeriesList />} />
-            <Route path="/add" element={<MangaSearch />} />
-            <Route path="/series/:id" element={<VolumeList />} />
-            <Route path="/settings" element={<MangaSettings />} />
-          </Routes>
-        </main>
-
-        {/* Footer */}
-        <footer className="footer">
-          <p>MangaArr v0.1.0 — Manga Download Manager</p>
-        </footer>
-      </div>
-    </Router>
+    <DocumentTitle title={window.Readarr.instanceName}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <ApplyTheme>
+            <PageConnector>
+              <AppRoutes app={App} />
+            </PageConnector>
+          </ApplyTheme>
+        </ConnectedRouter>
+      </Provider>
+    </DocumentTitle>
   );
+}
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 export default App;
