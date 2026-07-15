fa08148bf7a2296b01f4f25c85cb7f46087cc14a
Mark Vital <mark@vital.com>
2026-07-15 01:25:31 -0500

    Restore Sentry source files - DO NOT TOUCH SENTRY AGAIN

--- src/NzbDrone.Common/Instrumentation/InitializeLogger.cs
+++ src/NzbDrone.Common/Instrumentation/InitializeLogger.cs
@@ -1,4 +1,7 @@
+﻿using System.Linq;
+using NLog;
 using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Common.Instrumentation.Sentry;
 
 namespace NzbDrone.Common.Instrumentation
 {
@@ -13,6 +16,11 @@ namespace NzbDrone.Common.Instrumentation
 
         public void Initialize()
         {
+            var sentryTarget = LogManager.Configuration.AllTargets.OfType<SentryTarget>().FirstOrDefault();
+            if (sentryTarget != null)
+            {
+                sentryTarget.UpdateScope(_osInfo);
+            }
         }
     }
 }
--- src/NzbDrone.Core/Instrumentation/ReconfigureLogging.cs
+++ src/NzbDrone.Core/Instrumentation/ReconfigureLogging.cs
@@ -7,6 +7,7 @@ using NLog.Targets.Syslog.Settings;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation;
+using NzbDrone.Common.Instrumentation.Sentry;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Configuration.Events;
 using NzbDrone.Core.Datastore;
@@ -61,6 +62,9 @@ namespace NzbDrone.Core.Instrumentation
             //Log Sql
             SqlBuilderExtensions.LogSql = _configFileProvider.LogSql;
 
+            //Sentry
+            ReconfigureSentry();
+
             LogManager.ReconfigExistingLoggers();
         }
 
@@ -92,6 +96,16 @@ namespace NzbDrone.Core.Instrumentation
             foreach (var target in LogManager.Configuration.AllTargets.OfType<NzbDroneFileTarget>())
             {
                 target.MaxArchiveFiles = _configFileProvider.LogRotate;
+            }
+        }
+
+        private void ReconfigureSentry()
+        {
+            var sentryTarget = LogManager.Configuration.AllTargets.OfType<SentryTarget>().FirstOrDefault();
+            if (sentryTarget != null)
+            {
+                sentryTarget.SentryEnabled = (RuntimeInfo.IsProduction && _configFileProvider.AnalyticsEnabled) || RuntimeInfo.IsDevelopment;
+                sentryTarget.FilterEvents = _configFileProvider.FilterSentryEvents;
             }
         }
 
