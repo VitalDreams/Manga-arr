38b340d0f01f9240035ca24ba6b38e4b0fb85932
Mark Vital <mark@vital.com>
2026-07-15 04:03:08 -0500

    Fix Sentry strip script - use exact method signature for regex match

--- scripts/strip-sentry.sh
+++ scripts/strip-sentry.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 # Remove Sentry from NzbDroneLogger.cs
 FILE="NzbDrone.Common/Instrumentation/NzbDroneLogger.cs"
-python3 -c "
+python3 << 'PYEOF'
 import re
-f='$FILE'
-c=open(f).read()
-c=re.sub(r'using NzbDrone\.Common\.Instrumentation\.Sentry;\n','',c)
-c=re.sub(r'\s*RegisterSentry\(updateApp, appFolderInfo\);\n','',c)
-c=re.sub(r'\s*private static void RegisterSentry\(.*?\n\s*\}\n','',c,flags=re.DOTALL)
-open(f,'w').write(c)
-"
+f = "NzbDrone.Common/Instrumentation/NzbDroneLogger.cs"
+c = open(f).read()
+# Remove using statement
+c = re.sub(r'using NzbDrone\.Common\.Instrumentation\.Sentry;\n', '', c)
+# Remove RegisterSentry call
+c = re.sub(r'\s*RegisterSentry\(updateApp, appFolderInfo\);\n', '', c)
+# Remove RegisterSentry method (from line 65 to line 93 inclusive)
+c = re.sub(r'        private static void RegisterSentry\(bool updateClient, IAppFolderInfo appFolderInfo\)\n        \{\n.*?        \}\n', '', c, flags=re.DOTALL)
+open(f, 'w').write(c)
+PYEOF
+
 # Remove Sentry from InitializeLogger.cs
 sed -i '/using NzbDrone.Common.Instrumentation.Sentry;/d' NzbDrone.Common/Instrumentation/InitializeLogger.cs
 sed -i '/var sentryTarget/,/}/d' NzbDrone.Common/Instrumentation/InitializeLogger.cs
+
 # Remove Sentry from ReconfigureLogging.cs
 sed -i '/using NzbDrone.Common.Instrumentation.Sentry;/d' NzbDrone.Core/Instrumentation/ReconfigureLogging.cs
 sed -i '/ReconfigureSentry();/d' NzbDrone.Core/Instrumentation/ReconfigureLogging.cs
-sed -i '/private void ReconfigureSentry/,/^[[:space:]]*private void SetSyslog/ { /^[[:space:]]*private void SetSyslog/!d; }' NzbDrone.Core/Instrumentation/ReconfigureLogging.cs
+python3 << 'PYEOF'
+import re
+f = "NzbDrone.Core/Instrumentation/ReconfigureLogging.cs"
+c = open(f).read()
+c = re.sub(r'\s*private void ReconfigureSentry\(\)\s*\{.*?\s*\}\n', '', c, flags=re.DOTALL)
+open(f, 'w').write(c)
+PYEOF
